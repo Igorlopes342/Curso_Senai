e62907de5896db5de31e02f148b279ddfe930138
Igorlopes342 <kigorlopes@gmail.com>
2019-06-18 15:13:35 -0300

    Alteração Cliente

--- src/main/java/com/Igorsenai/services/ClienteService.java
+++ src/main/java/com/Igorsenai/services/ClienteService.java
@@ -1,22 +1,50 @@
 package com.Igorsenai.services;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.Igorsenai.domain.Cliente;
 import com.Igorsenai.exceptions.ObjectNotFoundException;
 import com.Igorsenai.repositories.ClienteRepository;
+import com.Igorsenai.repositories.EnderecoRepository;
 
 @Service
 public class ClienteService {
 
+	@Autowired
 	private ClienteRepository repo;
+
+	@Autowired
+	EnderecoRepository enderecoRepository;
 
 	public Cliente buscar(Integer idCliente) {
 		Optional<Cliente> obj = repo.findById(idCliente);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Número de id não encontrado. Id: " + idCliente + "tipo: " + Cliente.class.getName()));
+
 	}
 
+	public List<Cliente> findAll() {
+		return repo.findAll();
+
+	}
+
+	public Cliente insert(Cliente obj) {
+		obj.setIdCliente(null);
+		obj = repo.save(obj);
+
+		enderecoRepository.saveAll(obj.getEnderecos());
+
+		return obj;
+
+	}
+
+	public Cliente update(Cliente obj) {
+		buscar(obj.getIdCliente());
+		return repo.save(obj);
+
+	}
 }
